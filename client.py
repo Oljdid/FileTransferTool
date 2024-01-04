6626a11c56dc38c4f3c1e4c6108cd717006ec5ba
Shadowstrikesblu <hmoulk8@gmail.com>
2024-01-04 19:50:23 +0100

    04/01

--- client.py
+++ client.py
@@ -1,6 +1,7 @@
 import socket
 from scapy.all import DNS, DNSQR, IP, send, UDP
 import binascii
+import os
 
 def display_menu():
     print("1. Send a file by DNS ")
@@ -11,18 +12,12 @@ def file_to_hex(file_path):
     with open(file_path, 'rb') as file:
         binary_data = file.read()
     return binascii.hexlify(binary_data).decode()
-# print(hex_data)
 
 def send_file_dns(server_ip, file_path):
-
-    # Open the file in binary mode
-    with open(file_path, 'rb') as file:
-        hex_data = file_to_hex(file_path)
-        file_data = hex_data
-        # file_data = file.read()
+    hex_data = file_to_hex(file_path)
 
     # Encode the file data into a domain name
-    domain = file_data + '.example.com'
+    domain = hex_data + '.example.com'
 
     # Create a DNS query with the domain name
     packet = IP(dst=server_ip) / UDP() / DNS(rd=1, qd=DNSQR(qname=domain))
@@ -30,36 +25,37 @@ def send_file_dns(server_ip, file_path):
     # Send the packet
     send(packet)
 
+def send_file_imcp():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.bind(("127.0.0.1", 12345)) #if the clients/server are on different network you shall bind to ('', port)
+
+    s.listen(10)
+    c, addr = s.accept()
+    print('{} connected.'.format(addr))
+
+    f = open("file.txt", "rb")
+    l = os.path.getsize("file.txt")
+    m = f.read(l)
+    c.sendall(m)
+    f.close()
+    print("Terminé...")
+
 
 def main():
     while True:
         display_menu()
         choice = input("Enter your choice: ")
         if choice == "1":
-            send_file_dns('192.168.1.1', 'file.txt')
+            server_ip = input("Enter server IP: ")
+            send_file_dns(server_ip, 'file.txt')
         elif choice == "2":
-            send_file_imcp('localhost', 'file.txt')
-
+            # server_ip = input("Enter server IP: ")
+            # send_file_imcp('', 'file.txt')
+            send_file_imcp()
         elif choice == "3":
             print("Exit")
             break
         else:
-            print("Invalid choice")
-
-
-def send_file_imcp(server_ip, file_path):
-    # Create a socket object
-    client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-    # Connect to the server
-    client_socket.connect((server_ip, 12345))
-
-    # Open the file in binary mode and send its contents
-    with open(file_path, 'rb') as file:
-        client_socket.sendall(file.read())
-
-    # Close the connection
-    client_socket.close()
-
+            print("Invalid choice")        
 
 main()
--- server.py
+++ server.py
@@ -24,31 +24,25 @@ def receive_file_dns(packet):
 
 
 
-def receive_file_imcp(server_ip, server_port, file_path):
-    # Create a TCP/IP socket
-    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+def receive_file_imcp():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect(("127.0.0.1", 12345)) 
 
-    # Bind the socket to the address
-    server_socket.bind((server_ip, server_port))
-
-    # Listen for incoming connections
-    server_socket.listen(1)
-
+    f = open("recieved.txt", "wb")
+    data = None
     while True:
-        # Wait for a connection
-        print('waiting for a connection')
-        client_socket, client_address = server_socket.accept()
+        m = s.recv(1024)
+        data = m
+        if m:
+            while m:
+                m = s.recv(1024)
+                data += m
+            else:
+                break
+    f.write(data)
+    f.close()
+    print("Done receiving")
 
-        # Receive the file and write it to disk
-        with open(file_path, 'wb') as file:
-            while True:
-                data = client_socket.recv(1024)
-                if not data:
-                    break
-                file.write(data)
-
-        # Clean up the connection
-        client_socket.close()
 def display_menu():
     print("1. Receive a file by DNS ")
     print("2. Receive a file by IMCP ")
@@ -61,7 +55,7 @@ def main():
         if choice == "1":
             sniff(filter='udp port 53', prn=receive_file_dns)
         elif choice == "2":
-            receive_file_imcp('localhost', 12345, 'received_file.txt')
+            receive_file_imcp()
 
         elif choice == "3":
             print("Exit")
